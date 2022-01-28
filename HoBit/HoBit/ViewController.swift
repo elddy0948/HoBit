0f134353f542c2cf26d4b402280e7e4dce4b79da
Joons <elddy0948@gmail.com>
2022-01-28 12:42:55 +0900

    project init

---
+++ HoBit/HoBit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HoBit
+//
+//  Created by 김호준 on 2022/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPurple
+  }
+
+
+}
+
